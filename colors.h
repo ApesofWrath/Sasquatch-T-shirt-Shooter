90b8842673f111f364463db70d4dfcb3c70bb1de
mark005 <lieb005@gmail.com>
2013-10-06 15:07:16 -0700

    Added Color support and added lots of colors to a new header
    Also added initial support for setting the

---
+++ colors.h
@@ -0,0 +1,57 @@
+/*
+ * colors.h
+ *
+ *  Created on: 06.10.2013
+ *      Author: mark
+ */
+
+#ifndef COLORS_H_
+#define COLORS_H_
+
+#define RED			(0xFF0000)
+#define GREEN		(0x00FF00)
+#define BLUE		(0x0000FF)
+
+#define DARK_RED	(0x800000)
+#define DARK_GREEN	(0x008000)
+#define DARK_BLUE	(0x000080)
+
+#define YELLOW		(0xFFFF00)
+#define CYAN		(0x00FFFF)
+#define MAGENTA		(0xFF00FF)
+
+#define ORANGE		(0xFF8000)
+#define AQUA		(0x00FF80)
+#define VIOLET		(0x8000FF)
+
+#define CHARTREUSE	(0x80FF00)
+#define SKY_BlUE	(0x0080FF)
+#define ROSE		(0xFF0080)
+
+#define OLIVE		(0x808000)
+#define TEAL		(0x008080)
+#define PURPLE		(0x800080)
+
+#define CORAL		(0xFF8080)
+#define LIME		(0x80FF80)
+#define LAVENDER	(0x8080FF)
+
+#define EGG			(0xFFFF80)
+#define PINK		(0xFF80FF)
+#define TEAL2		(0x80FFFF)
+
+#define BLACK		(0x000000)
+#define WHITE		(0xFFFFFF)
+
+#define GREY1		(0x191919)
+#define GREY2		(0x333333)
+#define GREY3		(0x4C4C4C)
+#define GREY4		(0x666666)
+#define GREY5		(0x808080)
+#define GREY6		(0x999999)
+#define GREY7		(0xB2B2B2)
+#define GREY8		(0xCCCCCC)
+#define GREY9		(0xD4D4D4)
+
+
+#endif /* COLORS_H_ */
